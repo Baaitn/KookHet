7d3d15f0aef8ec040e910737bc3c4f29a97fcd39
Bert <maerten.bert@outlook.com>
2014-12-03 05:01:16 +0100

    Check-In, Werkt maar nog geen tabs, geen image & aantalpersonen kan nog lager dan 1 Niet afbranchen!

---
+++ KookHet/app/src/main/java/be/howest/nmct/android/kookhet/database/ReceptIngredientenLoader.java
@@ -0,0 +1,92 @@
+package be.howest.nmct.android.kookhet.database;
+
+import android.content.AsyncTaskLoader;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+public class ReceptIngredientenLoader extends AsyncTaskLoader<Cursor> {
+
+    private Cursor mCursor;
+    private final String mReceptNaam;
+
+    public ReceptIngredientenLoader(Context context, String ReceptNaam) {
+        super(context);
+        this.mReceptNaam = ReceptNaam;
+    }
+
+    @Override
+    public Cursor loadInBackground() {
+        DatabaseHelper helper = DatabaseHelper.getInstance(getContext());
+
+        SQLiteDatabase database = helper.getWritableDatabase();
+
+        mCursor = database.rawQuery(
+                " SELECT *" +
+                " FROM Ingredienten I" +
+                " INNER JOIN ReceptIngredient RI ON (RI.riIngredientId = I._ID)" +
+                " INNER JOIN Recepten R ON (RI.riReceptId = R._ID)" +
+                " WHERE R.rNaam = ?", new String[] {"" + mReceptNaam});
+
+        mCursor.getCount();
+
+        return mCursor;
+    }
+
+    @Override
+    public void deliverResult(Cursor cursor) {
+        if (isReset()) {
+            if (cursor != null) {
+                cursor.close();
+            }
+            return;
+        }
+
+        Cursor oldCursor = mCursor;
+        mCursor = cursor;
+
+        if (isStarted()) {
+            super.deliverResult(cursor);
+        }
+
+        if (oldCursor != null && oldCursor != cursor && !oldCursor.isClosed()) {
+            oldCursor.close();
+        }
+    }
+
+    @Override
+    protected void onStartLoading() {
+        if(mCursor != null) {
+            deliverResult(mCursor);
+        }
+
+        if(takeContentChanged() || mCursor == null) {
+            forceLoad();
+        }
+    }
+
+    @Override
+    protected void onStopLoading() {
+        cancelLoad();
+    }
+
+    @Override
+    public void onCanceled(Cursor cursor) {
+        if (cursor != null && !cursor.isClosed()) {
+            cursor.close();
+        }
+    }
+
+    @Override
+    protected void onReset() {
+        super.onReset();
+
+        onStopLoading();
+
+        if (mCursor != null && !mCursor.isClosed()) {
+            mCursor.close();
+        }
+
+        mCursor = null;
+    }
+}
